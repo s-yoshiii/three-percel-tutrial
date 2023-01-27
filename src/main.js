c89adc92ac7de9b554b8f2a07164d8bdd6da74e8
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-27 13:31:02 +0900

    Starを追加

--- src/main.js
+++ src/main.js
@@ -32,20 +32,42 @@ class EarthObject {
   lightSet() {
     const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
     directionalLight.position.set(1, 1, 1);
-    this.scene.add(directionalLight);
     const pointLight = new THREE.PointLight(0xffffff, 2, 1000);
     pointLight.position.set(
       500 * Math.sin(Date.now() / 500),
       500 * Math.sin(Date.now() / 1000),
       500 * Math.cos(Date.now() / 500)
     );
-    this.scene.add(pointLight);
     const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
-    this.scene.add(pointLightHelper);
+    this.scene.add(directionalLight, pointLight, pointLightHelper);
+  }
+  createStarField() {
+    // x,y,z座標の値がランダムに入った配列を500個生成
+    const vertices = [];
+    for (let i = 0; i < 500; i++) {
+      const x = 3000 * (Math.random() - 0.5);
+      const y = 3000 * (Math.random() - 0.5);
+      const z = 3000 * (Math.random() - 0.5);
+      vertices.push(x, y, z);
+    }
+    // 形状データ
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(vertices, 3)
+    );
+    const material = new THREE.PointsMaterial({
+      size: 8,
+      color: 0xffffff,
+    });
+    const stars = new THREE.Points(geometry, material);
+    this.scene.add(stars);
   }
   tick() {
     this.rot += 0.5;
     const radian = (this.rot * Math.PI) / 180;
+    // カメラの見る位置を固定する
+    this.camera.lookAt(new THREE.Vector3(0, 0, -400));
     // 角度に応じてカメラの位置を変更する
     this.camera.position.x = 1000 * Math.sin(radian);
     this.camera.position.z = 2000 * Math.cos(radian);
@@ -57,6 +79,7 @@ class EarthObject {
     this.setup();
     this.createMesh();
     this.lightSet();
+    this.createStarField();
     this.tick();
   }
 }
